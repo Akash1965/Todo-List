0992609e897c1d7af7511cca6ae31a2c01232edf
Akash Gupta <akashg.official18@gmail.com>
2023-07-02 03:14:34 +0530

    Add remove element in the array of tasks

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,27 +1,35 @@
-import React, { useContext} from 'react'
+import React, { useContext, useEffect, useState} from 'react'
 import { ListContext, ListStateContext} from '../App'
 
 export const List = () => {
   const listTitle = useContext(ListContext)
   const setListTitle = useContext(ListStateContext)
-  // const list = listTitle.slice(1)
+  const [task, setTask] = useState([])
 
   const deleteTask = (count) => {
     console.log(listTitle)
     console.log(count)
     setListTitle(listTitle.splice(count, 1))
+    setListTitle(listTitle.map((item, index) => {
+      item.taskNo = index
+      return listTitle
+    }))
   }
+
+  useEffect(() => {
+    setTask(...listTitle)
+  }, [listTitle])
 
   return (
     <div className='bg-gray-200 text-black px-4 py-6 my-10 w-96'>
         <div className='flex justify-around flex-col'>
           <ul className='flex flex-col gap-y-2'>
             {
-              listTitle.slice(1).map((item, index)=>{
+              listTitle.slice(1).map((item, index) => {
                 return <li className='flex justify-around gap-y-2' key={index}>
                 <div className='font-semibold'>{item.taskNo}</div>
-              <div className='font-semibold text-red-700'>{item.taskTitle}</div>
-              <div className='bg-red-500 font-semibold text-white px-3 rounded-xl cursor-pointer hover:bg-red-700' onClick={() => deleteTask(index+1)}>Delete</div>
+                <div className='font-semibold text-red-700'>{item.taskTitle}</div>
+                <div className='bg-red-500 font-semibold text-white px-3 rounded-xl cursor-pointer hover:bg-red-700' onClick={() => deleteTask(index+1)}>Delete</div>
                 </li>
                 })
             }
